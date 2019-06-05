6153ea632fb117b1c825300611770c37031a1a0b
Agata <agata.weltrowska@outlook.com>
2019-06-05 18:50:10 +0200

    zadanie nr 1

---
+++ src/main/java/com/javagda25/comparator/zadanie1/Osoba.java
@@ -0,0 +1,12 @@
+package com.javagda25.comparator.zadanie1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Osoba {
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+}
---
+++ src/main/java/com/javagda25/comparator/zadanie1/OsobaComparator.java
@@ -0,0 +1,15 @@
+package com.javagda25.comparator.zadanie1;
+
+import java.util.Comparator;
+
+public class OsobaComparator implements Comparator<Osoba> {
+
+    public int compare(Osoba o1, Osoba o2) {
+        if (o1.getWiek() < o2.getWiek()) {
+            return 1;
+        } else if (o1.getWiek() > o2.getWiek()) {
+            return -1;
+        }
+        return 0;
+    }
+}
