924a1453e531f22dbaa35292e91eccd8d93fa8ab
Agata <agata.weltrowska@outlook.com>
2019-06-05 19:16:09 +0200

    Skrócona klasa Main - bez tworzenia klasy OsobaComparator

---
+++ src/main/java/com/javagda25/comparator/zadanie1/MainSkrocony.java
@@ -0,0 +1,27 @@
+package com.javagda25.comparator.zadanie1;
+
+// Stwórz klasę Osoba (String imie, String nazwisko, int wiek), a następnie stwórz dla niego Comparator,
+// który sortuje osoby po wieku.
+// W mainie stwórz kilka instancji klasy osoba i dodaj je do Listy. Po dodaniu posortuj listę i na ekran
+// wypisz  wynik przed sortowaniem, oraz po sortowaniu.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainSkrocony {
+    public static void main(String[] args) {
+        List<Osoba> osobaList = new ArrayList<>();
+
+        osobaList.add(new Osoba("Ania", "Kowalska", 25));
+        osobaList.add(new Osoba("Anna", "Nowak", 28));
+        osobaList.add(new Osoba("Waldek", "Lis", 12));
+        osobaList.add(new Osoba("Kazik", "Kowalski", 45));
+        osobaList.add(new Osoba("Bogdan", "Kot", 23));
+
+        // klasa anonimowa
+        Collections.sort(osobaList, (o1,o2) -> Integer.compare(o1.getWiek(), o2.getWiek()));
+
+        System.out.println(osobaList);
+    }
+}
